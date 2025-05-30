7639536d27b30508915c89aaee80e1d1fa61191a
corona94 <octa9484@gmail.com>
2025-05-30 13:46:00 -0600

    First update

---
+++ src/test/java/BackendSpring/Administration/AdministrationApplicationTests.java
@@ -0,0 +1,13 @@
+package BackendSpring.Administration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdministrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
